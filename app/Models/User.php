2905332d68b28ef52e00d9ca0364500a8b48f1ee
hilman-firdd <hilmanfirdaus48@gmail.com>
2022-11-02 21:34:47 +0700

    #11 | make slug user

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use Sluggable;
 
     /**
      * The attributes that are mass assignable.
@@ -33,6 +35,15 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'username'
+            ]
+        ];
+    }
 
     /**
      * The attributes that should be cast.
